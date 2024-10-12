68beb2bfc6126af94e1795938fc877566f4d9a45
Christopher Young <c.t.ythegamer@gmail.com>
2024-10-11 20:21:56 -0600

    Projects added

---
+++ Proj07Blazor/Components/Pages/Joke.razor.cs
@@ -0,0 +1,12 @@
+﻿using Proj_06.Data;
+
+namespace Proj07Blazor.Components.Pages
+{
+    //public partial class Joke(HttpClient httpClient)
+    //{
+    //    protected override async Task OnInitializedAsync()
+    //    {
+    //        base.OnInitializedAsync();
+    //    }
+    //}
+}
